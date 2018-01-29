1e250177594882cf1a856508d4ff5b57ba2ce5e6
Aman020 <amanbhayana@yahoo.in>
2018-01-29 21:50:37 +0530

    Adding Project

---
+++ AzureSql/AzureSql/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AzureSql.MvcApplication" Language="C#" %>
---
+++ AzureSql/AzureSql/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzureSql.Startup))]
+namespace AzureSql
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
